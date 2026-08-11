1ad7328625d5d443739bf6005a67e148ec34a4df
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-11 11:33:38 -0300

    Delete PROJETOS directory

--- PROJETOS/SpringBoot/Tests_JUnit_Mockito/src/main/java/com/example/RestAPI/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.RestAPI.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String mensagem) {
-        super(mensagem);
-    }
-}
--- PROJETOS/SpringBoot/Url_Shortener_v2/src/main/java/com/example/urlshortener/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.urlshortener.exception;
-
-public class UrlNotFoundException extends RuntimeException {
-  public UrlNotFoundException(String message) {
-      super(message);
-  }
-}
