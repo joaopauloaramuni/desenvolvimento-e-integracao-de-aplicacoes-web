8927ead5c5c4820f1c0b828740213529eccf1ca8
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-24 07:29:15 -0300

    Refactor Controller to use dependency injection for Service

--- PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/controller/Controller.java
+++ PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/controller/Controller.java
@@ -1,13 +1,18 @@
 package com.example.RestAPI.controller;
 
-import com.example.RestAPI.service.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.RestAPI.service.Service;
 
 @RestController
 public class Controller {
 
-    Service service = new Service();
+    private final Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
 
     @GetMapping("/clima")
     public String preverTempo(){
