986476087f282d96601bdd0cb41694419cce32fd
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-24 07:23:37 -0300

    Update Controller.java

--- PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/controller/Controller.java
+++ PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/controller/Controller.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    private final Service service;
-
-    public Controller(Service service) {
-        this.service = service;
-    }
+    Service service = new Service();
 
     @GetMapping("/clima")
     public String preverTempo(){
