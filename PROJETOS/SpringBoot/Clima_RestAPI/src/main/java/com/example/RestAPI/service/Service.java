408382962a93177c694acbe70de0ba2eb81ff9af
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-24 07:28:13 -0300

    Update Service.java

--- PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
+++ PROJETOS/SpringBoot/Clima_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
@@ -1,8 +1,10 @@
 package com.example.RestAPI.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class Service {
 
     @Value("${climatempo.api.token}")
